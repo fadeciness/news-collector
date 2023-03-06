b6052b9c987179803f41529e60cff82a838cb919
fadeciness <solodimov.eugene@yandex.ru>
2023-03-06 22:37:23 +0300

    Telegram notification fixed

--- telegram-notification.py
+++ telegram-notification.py
@@ -4,26 +4,27 @@ import time
 
 import telebot
 
-con = sl.connect('news-collector.db')
-T = os.getenv('TELETOKEN')
-bot = telebot.TeleBot(T)
 
-# CHANNEL_NAME = '@FadecinessNews'
-CHANNEL_NAME = '@testfadeciness'
+def main():
+    con = sl.connect('news-collector.db')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    bot = telebot.TeleBot(telegram_token)
 
-with con:
-    res = con.execute("SELECT * FROM edisclosureru_history WHERE is_notification_send = 0 LIMIT 20")
-    for row in res:
-        # print(row)
-        message = 'id: ' + str(row[0]) + '\n' + \
-                  'Наименование компании: ' + str(row[4]) + '\n' + \
+    with con:
+        res = con.execute("SELECT * FROM edisclosureru_history WHERE is_notification_send = 0 LIMIT 10")
+        for row in res:
+            message = 'id: ' + str(row[0]) + '\n' + \
+                      'Наименование компании: ' + str(row[4]) + '\n' + \
                       'Дата публикации: ' + str(row[1]) + '\n' + \
-                  'Событие: ' + str(row[2]) + '\n' + \
-                  'Ссылка: ' + str(row[3])
-        bot.send_message(chat_id=CHANNEL_NAME, text=message)
-        print('Сообщение отправлено: ' + str(message))
-        con.execute("UPDATE edisclosureru_history SET is_notification_send = 1 WHERE id = " + str(row[0]))
-        print('Таблица edisclosureru_history обновлена - id: ' + str(row[0]))
-        time.sleep(10)
+                      'Событие: ' + str(row[2]) + '\n' + \
+                      'Ссылка: ' + str(row[3])
+            bot.send_message(chat_id=chat_id, text=message)
+            print('Сообщение отправлено: ' + str(message))
+            con.execute("UPDATE edisclosureru_history SET is_notification_send = 1 WHERE id = " + str(row[0]))
+            time.sleep(10)
 
-print('Program finished')
+
+if __name__ == '__main__':
+    main()
+    print("Notification sent")
