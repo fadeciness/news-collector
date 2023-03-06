86dc84cd6158f73d444041c144c121556fa31f77
fadeciness <solodimov.eugene@yandex.ru>
2023-03-06 20:42:01 +0300

    Small fixes

--- create-sqlite-db.py
+++ create-sqlite-db.py
@@ -1,80 +1,82 @@
 import sqlite3 as sl
 
-con = sl.connect('news-collector.db')
 
-# TABLE: tracked companies from e-disclosure.ru
-with con:
-    con.execute("""
-        CREATE TABLE IF NOT EXISTS edisclosureru (
-            id INTEGER PRIMARY KEY,
-            shortname VARCHAR(255),
-            ticker VARCHAR(30),
-            inn VARCHAR(12),
-            site_company_id VARCHAR(255)
-        );
-    """)
+def main():
+    con = sl.connect('news-collector.db')
 
-with con:
-    con.execute("DELETE FROM edisclosureru")
+    # TABLE: tracked companies from e-disclosure.ru
+    print("Database initializing")
+    with con:
+        con.execute("""
+            CREATE TABLE IF NOT EXISTS edisclosureru (
+                id INTEGER PRIMARY KEY,
+                shortname VARCHAR(255),
+                ticker VARCHAR(30),
+                inn VARCHAR(12),
+                site_company_id VARCHAR(255)
+            );
+        """)
+    print("Table 'edisclosureru' created")
 
-sql = 'INSERT INTO edisclosureru (shortname, ticker, inn, site_company_id) VALUES (?, ?, ?, ?)'
-data = [
-    ('ПАО "Газпром нефть"', "MOEX:SIBN", "5504036333", "347"),
-    ('ПАО Сбербанк', "MOEX:SBER", "7707083893", "3043")
-]
-with con:
-    con.executemany(sql, data)
+    with con:
+        con.execute("DELETE FROM edisclosureru")
+    print("Table 'edisclosureru' cleaned out")
 
-with con:
-    print("Table 'edisclosureru'")
-    data = con.execute("SELECT * FROM edisclosureru")
-    for row in data:
-        print(row)
+    sql = 'INSERT INTO edisclosureru (shortname, ticker, inn, site_company_id) VALUES (?, ?, ?, ?)'
+    data = [
+        ('ПАО "Газпром нефть"', "MOEX:SIBN", "5504036333", "347"),
+        ('ПАО Сбербанк', "MOEX:SBER", "7707083893", "3043")
+    ]
+    with con:
+        con.executemany(sql, data)
+    print("Table 'edisclosureru' filled in")
 
-# TABLE: history of articles
-with con:
-    con.execute("""
-        CREATE TABLE IF NOT EXISTS edisclosureru_history (
-            id INTEGER PRIMARY KEY,
-            publication_date VARCHAR(30) NOT NULL,
-            event_name VARCHAR(255) NOT NULL,
-            event_url VARCHAR(255) NOT NULL,
-            shortname VARCHAR(255) NOT NULL,
-            site_company_id VARCHAR(255) NOT NULL,
-            is_notification_send INTEGER DEFAULT 0 NOT NULL,
-            FOREIGN KEY(shortname) REFERENCES edisclosureru(shortname),
-            FOREIGN KEY(site_company_id) REFERENCES edisclosureru(site_company_id)
-        );
-    """)
+    with con:
+        print("Table 'edisclosureru':")
+        data = con.execute("SELECT * FROM edisclosureru")
+        for row in data:
+            print(row)
 
-with con:
-    con.execute("DELETE FROM edisclosureru_history")
+    # TABLE: history of articles
+    with con:
+        con.execute("""
+            CREATE TABLE IF NOT EXISTS edisclosureru_history (
+                id INTEGER PRIMARY KEY,
+                publication_date VARCHAR(30) NOT NULL,
+                event_name VARCHAR(255) NOT NULL,
+                event_url VARCHAR(255) NOT NULL,
+                shortname VARCHAR(255) NOT NULL,
+                site_company_id VARCHAR(255) NOT NULL,
+                is_notification_send INTEGER DEFAULT 0 NOT NULL,
+                FOREIGN KEY(shortname) REFERENCES edisclosureru(shortname),
+                FOREIGN KEY(site_company_id) REFERENCES edisclosureru(site_company_id)
+            );
+        """)
+    print("Table 'edisclosureru_history' created")
 
-with con:
-    print("Table 'edisclosureru_history'")
-    data = con.execute("SELECT * FROM edisclosureru_history")
-    for row in data:
-        print(row)
+    with con:
+        con.execute("DELETE FROM edisclosureru_history")
+    print("Table 'edisclosureru_history' cleaned out")
 
-# TEMPORARY TABLE: tmp history of articles
-with con:
-    con.execute("""
-        CREATE TABLE IF NOT EXISTS tmp_edisclosureru_history (
-            publication_date VARCHAR(30) NOT NULL,
-            event_name VARCHAR(255) NOT NULL,
-            event_url VARCHAR(255) NOT NULL,
-            shortname VARCHAR(255) NOT NULL,
-            site_company_id VARCHAR(255) NOT NULL,
-            FOREIGN KEY(shortname) REFERENCES edisclosureru(shortname),
-            FOREIGN KEY(site_company_id) REFERENCES edisclosureru(site_company_id)
-        );
-    """)
+    # TEMPORARY TABLE: tmp history of articles
+    with con:
+        con.execute("""
+            CREATE TABLE IF NOT EXISTS tmp_edisclosureru_history (
+                publication_date VARCHAR(30) NOT NULL,
+                event_name VARCHAR(255) NOT NULL,
+                event_url VARCHAR(255) NOT NULL,
+                shortname VARCHAR(255) NOT NULL,
+                site_company_id VARCHAR(255) NOT NULL,
+                FOREIGN KEY(shortname) REFERENCES edisclosureru(shortname),
+                FOREIGN KEY(site_company_id) REFERENCES edisclosureru(site_company_id)
+            );
+        """)
+    print("Table 'tmp_edisclosureru_history' created")
 
-with con:
-    con.execute("DELETE FROM tmp_edisclosureru_history")
+    with con:
+        con.execute("DELETE FROM tmp_edisclosureru_history")
+    print("Table 'tmp_edisclosureru_history' cleaned out")
 
-with con:
-    print("Table 'tmp_edisclosureru_history'")
-    data = con.execute("SELECT * FROM tmp_edisclosureru_history")
-    for row in data:
-        print(row)
+
+if __name__ == '__main__':
+    main()
